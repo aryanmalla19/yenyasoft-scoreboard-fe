fdd7816de7d89f6719099fe81a8fda2bb9f81c1b
Aryan Malla <aryanmalla19@gmail.com>
2025-11-22 20:26:01 +0545

    update link for live match card

--- src/components/LiveMatchCard.vue
+++ src/components/LiveMatchCard.vue
@@ -9,7 +9,7 @@
 </script>
 
 <template>
-    <div class="bg-red-50 border-red-100 border rounded-2xl shadow-md p-6 hover:shadow-lg transition cursor-pointer">
+    <a :href="'/match/live/'+match.id" class="bg-red-50 border-red-100 border rounded-2xl shadow-md p-6 hover:shadow-lg transition cursor-pointer">
         <div class="flex justify-end items-center pb-3">
             <span class="text-xs px-3 py-1 rounded-full bg-red-100 text-red-700">● LIVE</span>
         </div>
@@ -27,5 +27,5 @@
                 <div class="text-xs mt-1">{{  match.away_team.name }}</div>
             </div>
         </div>
-    </div>
+    </a>
 </template>
